91a50a50f0547a71a7600fd7bc8d9e0489d257e5
janet <oluwafemijanet85@gmail.com>
2025-02-11 20:23:43 +0100

    commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Index from "./component/Index.jsx";
+import {StrictMode} from "react";
+
+function App() {
+
+    return (
+        <StrictMode>
+            <Index/>
+        </StrictMode>
+    )
+}
+
+export default App
